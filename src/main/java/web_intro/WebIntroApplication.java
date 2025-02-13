a6ef5f868a260d7f320f1cc70e79a4fe127acc11
rus1980707 <rus1980707@gmail.com>
2025-02-13 18:00:06 +0200

    web

---
+++ src/main/java/web_intro/WebIntroApplication.java
@@ -0,0 +1,13 @@
+package web_intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebIntroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web_intro/WebIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package web_intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
